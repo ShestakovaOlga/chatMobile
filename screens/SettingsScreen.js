05d746da9217ac9644017f4813e23f72f4c76ae4
Olga Shestakova <olgashestakova@MacBook-Air.home>
2019-12-01 17:49:39 +0100

    add Cerrar sesión

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -8,10 +8,27 @@ import {
     View,
     InputEvent,
 } from 'react-native';
+import { Logout } from '../server';
 
-export default function SettingsScreen() {
+export default function SettingsScreen(props) {
     return (
         <ScrollView>
+            <View style={{ flexGrow: 1 }}>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+                <Text>Aqui va Cambiar el nombre</Text>
+            </View>
+            <TouchableOpacity onPress={async () => {
+                await Logout()
+                props.navigation.navigate('Login')
+            }}>
+                <Text style={{ color: 'red', fontSize: 17 }}>Cerrar la sesión</Text>
+            </TouchableOpacity>
         </ScrollView>
     );
 }
